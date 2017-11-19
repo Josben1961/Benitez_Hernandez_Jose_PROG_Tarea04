b0aeae96b27b4e0b1ca827dd5895f3531f994dd0
José S. Benítez Hernández <josben@movistar.es>
2017-11-19 08:22:55 +0100

    Creamos un método toString para representar el punto

--- src/arrays/Punto.java
+++ src/arrays/Punto.java
@@ -1,12 +1,12 @@
 package arrays;
 
 public class Punto {
-	 private double x, y;
-	 
-	 public Punto(double x, double y) {
-		 this.x=x;
-		 this.y=y;
-	 }
+	private double x, y;
+
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
 	public double getX() {
 		return x;
@@ -14,6 +14,11 @@ public class Punto {
 
 	public double getY() {
 		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "Punto [x=" + x + ", y=" + y + "]";
 	}
 
 }
