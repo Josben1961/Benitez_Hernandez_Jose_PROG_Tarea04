f7552d70d85bfc2fc4b2d38c19dac317df6d3c5a
José S. Benítez Hernández <josben@movistar.es>
2017-11-19 15:28:55 +0100

    Creamos el método distancia (distancia entra punto y otro pasad por parámetro

--- src/arrays/Punto.java
+++ src/arrays/Punto.java
@@ -20,5 +20,14 @@ public class Punto {
 	public String toString() {
 		return "Punto [x=" + x + ", y=" + y + "]";
 	}
+	public double distancia(Punto puntoParam) {
+		 double x1=x;
+		 double y1=y;
+		 double x2 = puntoParam.getX();
+		 double y2 = puntoParam.getY();
+		
+		 double distancia = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		 return distancia;
+	 }
 
 }
