17e30ad9c325dcd719a51a1e7adef962a8d5df32
José S. Benítez Hernández <josben@movistar.es>
2017-11-19 15:59:50 +0100

    Creamos un constructor que acepte como parámetros los vértices y los asigne a los atributos

--- src/arrays/Triangulo.java
+++ src/arrays/Triangulo.java
@@ -2,8 +2,14 @@ package arrays;
 
 public class Triangulo {
 	
-	private Punto A;
-	private Punto B;
-	private Punto C;
+	private Punto vertA;
+	private Punto vertB;
+	private Punto vertC;
+	
+	public Triangulo(Punto vertA, Punto vertB, Punto vertC) {
+		this.vertA=vertA;
+		this.vertB=vertB;
+		this.vertC=vertC;
+	}
 
 }
