dd9fc78710fddff4c492be2f32fc5081f7d6e892
José S. Benítez Hernández <josben@movistar.es>
2017-11-23 09:03:52 +0100

    Creo el método toString

--- src/arrays/Triangulo.java
+++ src/arrays/Triangulo.java
@@ -1,15 +1,15 @@
 package arrays;
 
 public class Triangulo {
-	
+
 	private Punto vertA;
 	private Punto vertB;
 	private Punto vertC;
-	
+
 	public Triangulo(Punto vertA, Punto vertB, Punto vertC) {
-		this.vertA=vertA;
-		this.vertB=vertB;
-		this.vertC=vertC;
+		this.vertA = vertA;
+		this.vertB = vertB;
+		this.vertC = vertC;
 	}
 
 	public Punto getVertA() {
@@ -23,6 +23,18 @@ public class Triangulo {
 	public Punto getVertC() {
 		return vertC;
 	}
-	
 
+	@Override
+	public String toString() {
+		return "Triangulo [vertA=" +vertA.toString() + ", vertB=" + vertB.toString() +
+				", vertC=" + vertC.toString() + "]";
+	}
+	public double perimetro() {
+		double ladoAB = vertA.distancia(vertB);
+		double ladoBC = vertB.distancia(vertC);
+		double ladoCA = vertC.distancia(vertA);
+		double perimetro = (ladoAB + ladoBC + ladoCA);
+		return perimetro;
+	} 
+		
 }
