83afd93016f00a41fbccfded6ca5c8fab7f0fae9
José S. Benítez Hernández <josben@movistar.es>
2017-11-23 09:22:04 +0100

    Array para pedir los triángulos a generar

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -1,9 +1,46 @@
 package arrays;
 
+import utilidades.Entrada;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		//Variables
+		int cantTriangulos;		
+		int contPuntos = 0;
+		int mayorQueMedia = 0;
+		int menorQueMedia = 0;
+		double periSuma = 0;
+		double periMayor = 0;
+		double periMenor = 0;
+		double perimMedia;
+		do {			
+			System.out.println("Introduzca un número de triángulos");
+			cantTriangulos = Entrada.entero();
+		} while (cantTriangulos < 0);
 		
+		/*Creamos un array para calcular los puntos (multiplico la cantidad 
+		 * de triángulos introducidos por 3 puntos que tiene cada triángulo)
+		 */
+		Punto[] arrayPuntos = new Punto[cantTriangulos * 3];
+		
+		//Relleno el array con puntos aleatorios
+		for (int i = 0; i < arrayPuntos.length; i++) {
+			arrayPuntos[i] = new Punto((double) (Math.random() * 10), (double) (Math.random() * 10));
+		}
+		
+		//Creo el array para la cantidad de triángulos
+		Triangulo[] arrayTriangulos = new Triangulo[cantTriangulos];
+		
+				
+				// Definición de los puntos de mi triángulo
+				Punto A = new Punto(0, 5);
+
+				Punto B = new Punto(10, 14);
+
+				Punto C = new Punto(7, 15);
+
+				Triangulo miTriangulo = new Triangulo(A, B, C);
 
 	}
 
